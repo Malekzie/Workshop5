21fd517299e8853493a2bb30a1ffe4d33ff19467
Robbie Soriano <20562606-grapeberryfruit@users.noreply.gitlab.com>
2024-07-18 19:45:19 -0600

    Initial Commit

---
+++ TravelExperts.Utils/StaticDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExperts.Utils
+{
+    /// <summary>
+    /// Defines static values for the application
+    /// </summary>
+    public static class StaticDefinition
+    {
+        public const string Customer = "Customer";
+        public const string Admin = "Admin";
+    }
+}
