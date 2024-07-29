3799e22ccdb0bbd492705cfe6d4a2a759054dc8d
Robbie Soriano <grapeberryfruit@gmail.com>
2024-07-29 13:56:23 -0600

    idk

---
+++ TravelExperts.Models/ViewModel/CardVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExperts.Models.ViewModel
+{
+    public class CardVM
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
