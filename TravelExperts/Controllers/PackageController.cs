ae66b0c20f1d4142bb9c25cb690775683c1bb473
Ben Wood <benjamin.wood@edu.sait.ca>
2024-08-02 13:23:00 -0600

    Package page implemented
    route to package page created and package page setup.

--- TravelExperts/Controllers/PackageController.cs
+++ TravelExperts/Controllers/PackageController.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TravelExperts.DataAccess.Data;
+using TravelExperts.DataAccess.Models;
+using TravelExperts.Models;
 using TravelExperts.Models.ViewModel;
 
 namespace TravelExperts.Controllers
 {
     public class PackageController : Controller
     {
+        private TravelExpertsContext _context { get; set; }
+
+        public PackageController(TravelExpertsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             var cards = new List<CardVM>
@@ -12,28 +23,35 @@ namespace TravelExperts.Controllers
                 new CardVM
                 {
                     Name = "Card 1",
-                    ImageUrl = "https://via.placeholder.com/150",
+                    ImageUrl = "img/Caribbean3.jpg",
                     Desc = "This is a card"
                 },
                 new CardVM
                 {
                     Name = "Card 2",
-                    ImageUrl = "https://via.placeholder.com/150",
+                    ImageUrl = "img/Hawaii.jpg",
                     Desc = "This is a card"
                 },
                 new CardVM
                 {
                     Name = "Card 3",
-                    ImageUrl = "https://via.placeholder.com/150",
+                    ImageUrl = "img/Japan.jpg",
                     Desc = "This is a card"
                 },
                 new CardVM
                 {
                     Name = "Card 4",
-                    ImageUrl = "https://via.placeholder.com/150",
+                    ImageUrl = "img/Paris.jpg",
                     Desc = "This is a card"
                 }
             };
+            return View(cards);
+        }
+
+        public IActionResult BuyPackage()
+        {
+            var package = Package.GetPackage();
+            
             return View();
         }
     }
