eb664799f94add5944e86a00260861b02f305ea3
Robbie Soriano <20562606-grapeberryfruit@users.noreply.gitlab.com>
2024-08-03 01:40:59 -0600

    Migration

---
+++ TravelExperts.DataAccess/Migrations/20240803073742_CustomerTableEdit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelExperts.DataAccess.Migrations
+{
+    public partial class CustomerTableEdit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Add Username and Password columns to the existing Customers table
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Customers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Customers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Remove the Username and Password columns from the existing Customers table
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Customers");
+        }
+    }
+}
