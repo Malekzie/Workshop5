a918e814c0892447095153ada87da8d89f5d8124
Robbie Soriano <20562606-grapeberryfruit@users.noreply.gitlab.com>
2024-08-05 16:07:17 -0600

    homepage changes

--- TravelExperts/Controllers/HomeController.cs
+++ TravelExperts/Controllers/HomeController.cs
@@ -20,28 +20,29 @@ namespace TravelExperts.Controllers
             {
                 new CardVM
                 {
-                    Name = "Card 1",
-                    ImageUrl = "https://via.placeholder.com/150",
-                    Desc = "This is a card"
+                    Name = "Car Service",
+                    ImageUrl = "svg/car.svg",
+                    Desc = "In addition to our packages, we offer car rentals to ensure you have the freedom to explore your destination at your own pace. Whether it's a quick trip to the beach or a scenic drive through the countryside, our reliable vehicles will get you there comfortably."
                 },
                 new CardVM
                 {
-                    Name = "Card 2",
-                    ImageUrl = "https://via.placeholder.com/150",
-                    Desc = "This is a card"
+                    Name = "Tour Service",
+                    ImageUrl = "svg/tour.svg",
+                    Desc = "Enhance your travel experience with our high-quality tour guides. Our knowledgeable guides will introduce you to the rich history and hidden gems of your destination, ensuring you make the most out of your trip."
                 },
                 new CardVM
                 {
-                    Name = "Card 3",
-                    ImageUrl = "https://via.placeholder.com/150",
-                    Desc = "This is a card"
+                    Name = "Yacht / Boat Service",
+                    ImageUrl = "svg/yacht.svg",
+                    Desc = "Experience luxury on the water with our yacht and boat services. Whether you're looking to relax on a serene lake or sail across open seas, our well-equipped vessels offer the perfect escape for any adventurer."
                 },
-                new CardVM
-                {
-                    Name = "Card 4",
-                    ImageUrl = "https://via.placeholder.com/150",
-                    Desc = "This is a card"
-                }
+               new CardVM
+               {
+                    Name = "Travel Insurance",
+                    ImageUrl = "svg/insurance.svg",
+                    Desc = "Travel with peace of mind knowing you're covered. Our comprehensive travel insurance plans protect you from unexpected events, ensuring you can enjoy your trip without worry. From medical emergencies to trip cancellations, we've got you covered."
+               }
+
             };
 
 
