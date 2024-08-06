a235108a9d67e6996a7a0772f021853047b81b94
Robbie Soriano <grapeberryfruit@gmail.com>
2024-08-06 09:13:01 -0600

    bugfix

--- TravelExperts.Models/ViewModel/RegisterVM.cs
+++ TravelExperts.Models/ViewModel/RegisterVM.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace TravelExperts.Models.ViewModel
@@ -17,11 +18,9 @@ namespace TravelExperts.Models.ViewModel
 
         public class InputModel
         {
-            [EmailAddress]
-            public string Email { get; set; }
+            public string? Email { get; set; }
 
-            [Required(ErrorMessage = "Username is required.")]
-            public string Username { get; set; }
+            public string? Username { get; set; }
 
             [Required(ErrorMessage = "Password is required.")]
             [DataType(DataType.Password)]
@@ -54,9 +53,11 @@ namespace TravelExperts.Models.ViewModel
             [Required(ErrorMessage = "Country is required.")]
             public string CustCountry { get; set; }
 
-            [Required(ErrorMessage = "Home phone is required.")]
-            public string CustHomePhone { get; set; }
+            [DisplayName("Home Phone")]
+            public string? CustHomePhone { get; set; }
 
+            [DisplayName("Business Phone")]
+            [Required(ErrorMessage = "Business phone is required.")]
             public string CustBusPhone { get; set; }
         }
     }
